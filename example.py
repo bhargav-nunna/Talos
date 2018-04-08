19c12d436fabc0ebc3da3b00df3958dd83bf8e0b
bhargav-nunna <bhargav.vmf33@gmail.com>
2018-04-08 17:39:11 +0000

    Corrected code

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
 from irobot.robots.create2 import Create2
 from irobot.openinterface.constants import MODES
 # instantiate robot
-robot = Create2()
+port = '/dev/ttyUSB0'
+robot = Create2(port)
 # read sensor
 print(robot.left_encoder_counts)
 # change mode to drive (SAFE|FULL)
-robot.oi_mode = MODES.SAFE
+robot.oi_mode = MODES.FULL
 robot.drive_straight(50)
+#time.sleep( 5 ) 
 # stop driving
 robot.drive_straight(0)
 # return to passive mode
